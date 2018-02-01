5ba7e7ab08ad0975dd9701b5d02b1fea25ee069f
victorloo <ila9970@gmail.com>
2018-02-02 04:20:33 +0800

    format index and show output using jbuilder

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -3,15 +3,6 @@ class Api::V1::PhotosController < ApiController
   # GET http://localhost:3000/api/v1/photos
   def index
     @photos = Photo.all
-    render json: {
-      data: @photos.map do |photo|
-        {
-          title: photo.title,
-          date: photo.date,
-          description: photo.description
-        }
-      end
-    }
   end
 
   # GET http://localhost:3000/api/v1/photos/:id
@@ -23,11 +14,7 @@ class Api::V1::PhotosController < ApiController
         status: 400
       }
     else
-      render json: {
-        title: @photo.title,
-        date: @photo.date,
-        description: @photo.description
-      }
+      render "api/v1/photos/show"
     end
   end
   
